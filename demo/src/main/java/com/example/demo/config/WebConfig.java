765ab8669d2119ac9a0ae84ef4ef71076384de28
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-11-22 22:26:34 +0700

    nothing

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
    }
 
    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**").allowedOrigins(new String[]{"http://localhost:3000"}).allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"}).allowedHeaders(new String[]{"*"}).allowCredentials(true);
+      registry.addMapping("/**").allowedOrigins(new String[] { "http://localhost:3000" })
+            .allowedMethods(new String[] { "GET", "POST", "PUT", "DELETE" }).allowedHeaders(new String[] { "*" })
+            .allowCredentials(true);
    }
 }
