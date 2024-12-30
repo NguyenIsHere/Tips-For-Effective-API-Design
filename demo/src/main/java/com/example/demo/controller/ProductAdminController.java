70e220e06a0d28492cc275e6ae679116ca7a2748
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-12-31 01:42:00 +0700

    add header for cache

--- demo/src/main/java/com/example/demo/controller/ProductAdminController.java
+++ demo/src/main/java/com/example/demo/controller/ProductAdminController.java
@@ -16,115 +16,123 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ProductAdminController {
 
-   @Autowired
-   private ProductService productService;
+      @Autowired
+      private ProductService productService;
 
-   // Lấy danh sách tất cả sản phẩm
-   @GetMapping("/all")
-   public ResponseEntity<ApiResult<List<ProductDTO>>> getAllProducts(
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      List<ProductDTO> products = productService.getAllProducts();
-      ApiResult<List<ProductDTO>> result = new ApiResult<>(
-            true,
-            "Lấy danh sách sản phẩm thành công",
-            products);
-      return ResponseEntity.ok(result);
-   }
+      // Lấy danh sách tất cả sản phẩm
+      @GetMapping("/all")
+      public ResponseEntity<ApiResult<List<ProductDTO>>> getAllProducts(
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            List<ProductDTO> products = productService.getAllProducts();
+            ApiResult<List<ProductDTO>> result = new ApiResult<>(
+                        true,
+                        "Lấy danh sách sản phẩm thành công",
+                        products);
 
-   // Lấy 1 sản phẩm theo ID
-   @GetMapping("/{productId}")
-   public ResponseEntity<ApiResult<ProductDTO>> getProductById(
-         @PathVariable String productId,
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      ProductDTO product = productService.getProductById(productId);
-      if (product != null) {
-         ApiResult<ProductDTO> result = new ApiResult<>(
-               true,
-               "Lấy sản phẩm thành công",
-               product);
-         return ResponseEntity.ok(result);
-      } else {
-         ApiResult<ProductDTO> result = new ApiResult<>(
-               false,
-               "Không tìm thấy sản phẩm",
-               null);
-         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+            // Set Cache-Control headers
+            return ResponseEntity.ok()
+                        .header("Cache-Control", "public, max-age=600") // Cache for 10 minutes
+                        .body(result);
       }
-   }
 
-   // Tạo mới 1 sản phẩm
-   @PostMapping
-   public ResponseEntity<ApiResult<ProductDTO>> addProduct(
-         @RequestBody ProductDTO product,
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      ProductDTO savedProduct = productService.addProduct(product);
+      // Lấy 1 sản phẩm theo ID
+      @GetMapping("/{productId}")
+      public ResponseEntity<ApiResult<ProductDTO>> getProductById(
+                  @PathVariable String productId,
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            ProductDTO product = productService.getProductById(productId);
+            if (product != null) {
+                  ApiResult<ProductDTO> result = new ApiResult<>(
+                              true,
+                              "Lấy sản phẩm thành công",
+                              product);
 
-      // Trả về sản phẩm vừa tạo (hoặc bạn có thể trả về data=null nếu không cần)
-      ApiResult<ProductDTO> result = new ApiResult<>(
-            true,
-            "Tạo sản phẩm thành công",
-            savedProduct);
-
-      return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(result);
-   }
-
-   // Cập nhật màu sắc (color) của sản phẩm
-   @PostMapping("/{productId}/colors")
-   public ResponseEntity<ApiResult<ProductDTO>> updateProductColor(
-         @PathVariable String productId,
-         @RequestBody ColorUpdateRequest request,
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      String imageUrl = request.getImageUrl();
-      String colorCode = request.getColorCode();
-      List<Size> sizes = request.getSizes();
-
-      ProductDTO updatedProduct = productService.updateProductColor(productId, colorCode, imageUrl, sizes);
-
-      ApiResult<ProductDTO> result = new ApiResult<>(
-            true,
-            "Cập nhật màu cho sản phẩm thành công",
-            updatedProduct);
-
-      return ResponseEntity.ok(result);
-   }
-
-   // Cập nhật 1 sản phẩm
-   @PutMapping("/{productId}")
-   public ResponseEntity<ApiResult<ProductDTO>> updateProduct(
-         @PathVariable String productId,
-         @RequestBody ProductDTO product,
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      ProductDTO updatedProduct = productService.updateProduct(productId, product);
-
-      if (updatedProduct != null) {
-         ApiResult<ProductDTO> result = new ApiResult<>(
-               true,
-               "Cập nhật sản phẩm thành công",
-               updatedProduct);
-         return ResponseEntity.ok(result);
-      } else {
-         ApiResult<ProductDTO> result = new ApiResult<>(
-               false,
-               "Không tìm thấy sản phẩm để cập nhật",
-               null);
-         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+                  // Set Cache-Control headers
+                  return ResponseEntity.ok()
+                              .header("Cache-Control", "public, max-age=600") // Cache for 10 minutes
+                              .body(result);
+            } else {
+                  ApiResult<ProductDTO> result = new ApiResult<>(
+                              false,
+                              "Không tìm thấy sản phẩm",
+                              null);
+                  return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+            }
       }
-   }
 
-   // Xoá 1 sản phẩm
-   @DeleteMapping("/{productId}")
-   public ResponseEntity<ApiResult<Void>> deleteProduct(
-         @PathVariable String productId,
-         @RequestHeader("Authorization") String jwt) throws Exception {
-      productService.deleteProduct(productId);
+      // Tạo mới 1 sản phẩm
+      @PostMapping
+      public ResponseEntity<ApiResult<ProductDTO>> addProduct(
+                  @RequestBody ProductDTO product,
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            ProductDTO savedProduct = productService.addProduct(product);
 
-      // Chỉ cần trả về success & message, data = null
-      ApiResult<Void> result = new ApiResult<>(
-            true,
-            "Xóa sản phẩm thành công",
-            null);
-      return ResponseEntity.ok(result);
-   }
+            // Trả về sản phẩm vừa tạo (hoặc bạn có thể trả về data=null nếu không cần)
+            ApiResult<ProductDTO> result = new ApiResult<>(
+                        true,
+                        "Tạo sản phẩm thành công",
+                        savedProduct);
+
+            return ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body(result);
+      }
+
+      // Cập nhật màu sắc (color) của sản phẩm
+      @PostMapping("/{productId}/colors")
+      public ResponseEntity<ApiResult<ProductDTO>> updateProductColor(
+                  @PathVariable String productId,
+                  @RequestBody ColorUpdateRequest request,
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            String imageUrl = request.getImageUrl();
+            String colorCode = request.getColorCode();
+            List<Size> sizes = request.getSizes();
+
+            ProductDTO updatedProduct = productService.updateProductColor(productId, colorCode, imageUrl, sizes);
+
+            ApiResult<ProductDTO> result = new ApiResult<>(
+                        true,
+                        "Cập nhật màu cho sản phẩm thành công",
+                        updatedProduct);
+
+            return ResponseEntity.ok(result);
+      }
+
+      // Cập nhật 1 sản phẩm
+      @PutMapping("/{productId}")
+      public ResponseEntity<ApiResult<ProductDTO>> updateProduct(
+                  @PathVariable String productId,
+                  @RequestBody ProductDTO product,
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            ProductDTO updatedProduct = productService.updateProduct(productId, product);
+
+            if (updatedProduct != null) {
+                  ApiResult<ProductDTO> result = new ApiResult<>(
+                              true,
+                              "Cập nhật sản phẩm thành công",
+                              updatedProduct);
+                  return ResponseEntity.ok(result);
+            } else {
+                  ApiResult<ProductDTO> result = new ApiResult<>(
+                              false,
+                              "Không tìm thấy sản phẩm để cập nhật",
+                              null);
+                  return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+            }
+      }
+
+      // Xoá 1 sản phẩm
+      @DeleteMapping("/{productId}")
+      public ResponseEntity<ApiResult<Void>> deleteProduct(
+                  @PathVariable String productId,
+                  @RequestHeader("Authorization") String jwt) throws Exception {
+            productService.deleteProduct(productId);
+
+            // Chỉ cần trả về success & message, data = null
+            ApiResult<Void> result = new ApiResult<>(
+                        true,
+                        "Xóa sản phẩm thành công",
+                        null);
+            return ResponseEntity.ok(result);
+      }
 }
